ae7e9ff103bc0e25ad68ee12742ea2a08e50e066
jobo322 <jose.bolanos@correounivalle.edu.co>
2020-02-15 22:18:22 -0500

    add test case

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,7 +1,33 @@
-import { myModule } from '..';
+import { toBeDeepCloseTo } from 'jest-matcher-deep-close-to';
 
-describe('test myModule', () => {
-  it('should return 42', () => {
-    expect(myModule()).toEqual(42);
+import { gaussian, lorentzian, pseudoVoigt } from '..';
+
+expect.extend({ toBeDeepCloseTo });
+
+describe('Normalization', () => {
+  it('gaussian', () => {
+    let vector = gaussian({ factor: 50 });
+    let area = getArea(vector);
+    expect(area).toBeDeepCloseTo(1, 2);
+  });
+
+  it('lorentzian', () => {
+    let vector = lorentzian({ factor: 500 });
+    let area = getArea(vector);
+    expect(area).toBeDeepCloseTo(1, 2);
+  });
+
+  it('pseudoVoigt', () => {
+    let vector = pseudoVoigt({ factor: 50 });
+    let area = getArea(vector);
+    expect(area).toBeDeepCloseTo(1, 2);
   });
 });
+
+function getArea(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
