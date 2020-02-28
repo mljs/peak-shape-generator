7e91d842347ee25704df5e168eff4535f798acfd
jobo322 <jose.bolanos@correounivalle.edu.co>
2020-02-27 19:39:31 -0500

    close:https://github.com/mljs/peak-shape-generator/issues/9

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import { gaussian } from './gaussian';
 import { lorentzian } from './lorentzian';
 import { pseudoVoigt } from './pseudoVoigt';
-import { halfGaussian } from './halfGaussian';
-import { halfLorentzian } from './halfLorentzian';
-import { halfPseudoVoigt } from './halfPseudoVoigt';
 
-export {
-  gaussian,
-  lorentzian,
-  pseudoVoigt,
-  halfGaussian,
-  halfLorentzian,
-  halfPseudoVoigt,
-};
+export { gaussian, lorentzian, pseudoVoigt };
