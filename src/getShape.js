09b4ab89223f2c603eb76969c81f4c37823b22f8
Luc Patiny <luc@patiny.com>
2020-03-06 07:58:48 +0100

    fix: documentation

--- src/getShape.js
+++ src/getShape.js
@@ -1,12 +1,3 @@
-/**
- * Calculate a normalized gaussian shape
- * @param {number} [kind = 1]
- * @param {object} [options = {}]
- * @param {number} [options.fwhm = 500] - number of points in Full Width at Half Maximum, Standard deviation will be computed as fwhm / 2 / sqrt(2 ln(2))
- * @param {number} [options.factor = 3] - factor of standard deviation to increase the window size, the vector size is 2 * factor * sd
- * @return {Float64Array} - array of Y points
- */
-
 import { gaussian } from './gaussian';
 import { lorentzian } from './lorentzian';
 import { pseudoVoigt } from './pseudoVoigt';
@@ -14,6 +5,15 @@ import { pseudoVoigt } from './pseudoVoigt';
 export const GAUSSIAN = 1;
 export const LORENTZIAN = 2;
 export const PSEUDO_VOIGT = 3;
+
+/**
+ * Generate a shape of the specified kind
+ * @param {number} [kind = 1]
+ * @param {object} [options = {}]
+ * @param {number} [options.fwhm = 500] - number of points in Full Width at Half Maximum, Standard deviation will be computed as fwhm / 2 / sqrt(2 ln(2))
+ * @param {number} [options.factor = 3] - factor of standard deviation to increase the window size, the vector size is 2 * factor * sd
+ * @return {Float64Array} - array of Y points
+ */
 
 export function getShape(kind = 1, options = {}) {
   if (typeof kind === 'string') kind = getKind(kind);
