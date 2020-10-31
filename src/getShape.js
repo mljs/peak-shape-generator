ef56aad2ab1e60775e1483a21570b387e7cee200
Luc Patiny <luc@patiny.com>
2020-10-31 17:21:58 +0100

    chore: improve jsdoc

--- src/getShape.js
+++ src/getShape.js
@@ -8,7 +8,7 @@ export const PSEUDO_VOIGT = 3;
 
 /**
  * Generate a shape of the specified kind
- * @param {number} [kind = 1]
+ * @param {number|string} [kind = 1] kind may be 'gaussian', 'lorentzian' or 'pseudovoigt' or their corresponding number
  * @param {object} [options = {}]
  * @param {number} [options.fwhm = 500] - number of points in Full Width at Half Maximum, Standard deviation will be computed as fwhm / 2 / sqrt(2 ln(2))
  * @param {number} [options.factor = 3] - factor of standard deviation to increase the window size, the vector size is 2 * factor * sd
