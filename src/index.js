b82c926ae50d7b18643ffde35ccf266496f7aa74
jobo322 <jose.bolanos@correounivalle.edu.co>
2020-11-03 04:06:29 -0500

    chore: export shape funtions

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ export * from './lorentzian';
 export * from './pseudoVoigt';
 export * from './getShape';
 export * from './PeakShapeGenerator';
+export * from './shape/gaussian';
+export * from './shape/lorentzian';
+export * from './shape/pseudovoigt';
