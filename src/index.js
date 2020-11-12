997266e3e7ea340ec51c13352ea2d7f1c08636aa
jobo322 <jose.bolanos@correounivalle.edu.co>
2020-11-12 14:20:24 -0500

    feat: export getKind

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export * from './lorentzian';
 export * from './pseudoVoigt';
 export * from './getShape';
 export * from './getArea';
+export * from './util/getKind';
 export * from './PeakShapeGenerator';
 export * from './shapes/gaussianFct';
 export * from './shapes/lorentzianFct';
