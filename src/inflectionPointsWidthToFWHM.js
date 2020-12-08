790f9ecf937d4603d479b69bddbf64186341a5cd
Jose Alejandro Bolanos Arroyave <jose.bolanos@correounivalle.edu.co>
2020-12-08 10:12:09 -0500

    chore: reverse wrong editing documentation

--- src/inflectionPointsWidthToFWHM.js
+++ src/inflectionPointsWidthToFWHM.js
@@ -4,8 +4,8 @@ import { getKind, GAUSSIAN, LORENTZIAN, PSEUDO_VOIGT } from './util/getKind';
  * Compute the value of Full Width at Half Maximum (FWHM) of a specific shape from the width between the inflection points.
  * @param {number} width - Width between the inflection points
  * @param {number|string} [kind = 1] kind may be 'gaussian', 'lorentzian' or 'pseudovoigt' or their corresponding number
- * @param {*} options - options
- * @param {number|object} [options.mu = 0.5] - ratio of gaussian contribution. It is used if the kind = 3 or pseudovoigt.
+ * @param {object} options - options
+ * @param {number} [options.mu = 0.5] - ratio of gaussian contribution. It is used if the kind = 3 or pseudovoigt.
  */
 
 const lorentzianFactor = Math.sqrt(3);
