dc567c67db7a4cf22cb64d14c30a4c22a21f3e1d
jobo322 <jose.bolanos@correounivalle.edu.co>
2021-01-29 15:18:48 -0500

    feat: export Gaussian2D

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export * from './classes/Gaussian';
 export * from './classes/Lorentzian';
 export * from './classes/PseudoVoigt';
+export * from './classes/Gaussian2D';
 export * from './util/getShapeGenerator';
