3345373aa3ab5ceffeffc596886d21cec82efeb9
jose alejandro bolanos arroyave <jose.bolanos@correounivalle.edu.co>
2021-08-12 19:19:22 +0200

    fix: avoid import/named eslint error

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-export * as gaussian from './shapes/1d/gaussian/Gaussian';
-export * as lorentzian from './shapes/1d/lorentzian/Lorentzian';
-export * as pseudoVoigt from './shapes/1d/pseudoVoigt/PseudoVoigt';
-export * as gaussian2D from './shapes/2d/gaussian2D/Gaussian2D';
+import * as gaussian from './shapes/1d/gaussian/Gaussian';
+import * as lorentzian from './shapes/1d/lorentzian/Lorentzian';
+import * as pseudoVoigt from './shapes/1d/pseudoVoigt/PseudoVoigt';
+import * as gaussian2D from './shapes/2d/gaussian2D/Gaussian2D';
+
 export * from './util/getShape1D';
 export * from './util/getShape2D';
 
 export * from './shapes/1d/Shape1D';
 export * from './shapes/2d/Shape2D';
+
+export { gaussian, gaussian2D, lorentzian, pseudoVoigt };
 
 export type { XYNumber } from './shapes/2d/gaussian2D/Gaussian2D';
 export type { GaussianClassOptions } from './shapes/1d/gaussian/Gaussian';
