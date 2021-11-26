c4b94279f617fdb48275c4a0fd00e80797e81271
Luc Patiny <luc@patiny.com>
2021-11-26 22:47:51 +0100

    fix: expose static methods of Gaussian2D

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export * from './shapes/1d/gaussian/Gaussian';
 export * from './shapes/1d/lorentzian/Lorentzian';
 export * from './shapes/1d/pseudoVoigt/PseudoVoigt';
-export { Gaussian2D } from './shapes/2d/gaussian2D/Gaussian2D';
+export * from './shapes/2d/gaussian2D/Gaussian2D';
 
 export * from './shapes/1d/getShape1D';
 export * from './shapes/2d/getShape2D';
