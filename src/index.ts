ca527d1d48f87cd08a26eb8f075b3ab1c65651ae
Luc Patiny <luc@patiny.com>
2021-11-26 15:29:16 +0100

    chore: remove getShapeData1D and getShapeData2D

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 export { Gaussian } from './shapes/1d/gaussian/Gaussian';
 export { Lorentzian } from './shapes/1d/lorentzian/Lorentzian';
 export { PseudoVoigt } from './shapes/1d/pseudoVoigt/PseudoVoigt';
-export { Gaussian2D, XYNumber } from './shapes/2d/gaussian2D/Gaussian2D';
+export { Gaussian2D } from './shapes/2d/gaussian2D/Gaussian2D';
 
 export * from './shapes/1d/getShape1D';
 export * from './shapes/2d/getShape2D';
-export * from './shapes/1d/getShapeData1D';
-export * from './shapes/2d/getShapeData2D';
 
 export type { Shape1D } from './shapes/1d/Shape1D';
 export type { Shape2D } from './shapes/2d/Shape2D';
