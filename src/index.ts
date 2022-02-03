4e7892fe25be0831b3cf8c863193cb4824a9829f
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-02-03 15:03:59 +0100

    fix: export ShapeXDClass types

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,7 @@ export * from './shapes/2d/getShape2D';
 
 export type { Shape1D } from './shapes/1d/Shape1D';
 export type { Shape2D } from './shapes/2d/Shape2D';
+export type { Shape1DClass } from './shapes/1d/Shape1DClass';
+export type { Shape2DClass } from './shapes/2d/Shape2DClass';
 export type { Shape1DInstance } from './shapes/1d/Shape1DInstance';
 export type { Shape2DInstance } from './shapes/2d/Shape2DInstance';
