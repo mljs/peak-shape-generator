8d0e6076d828be9aa1275ce98382cefaf37215a9
Luc Patiny <luc@patiny.com>
2022-04-29 10:21:28 +0200

    test: check gaussian width

--- src/shapes/1d/gaussian/__tests__/Gaussian.fct.test.ts
+++ src/shapes/1d/gaussian/__tests__/Gaussian.fct.test.ts
@@ -1,4 +1,4 @@
-import { Gaussian } from '../Gaussian';
+import { Gaussian, getGaussianFactor } from '../Gaussian';
 
 describe('Gaussian function', () => {
   it('Gaussian.fct', () => {
@@ -11,5 +11,9 @@ describe('Gaussian function', () => {
     const gaussian = new Gaussian({ fwhm: 0.2 });
     expect(gaussian.fct(0)).toBeCloseTo(1);
     expect(gaussian.fct(0.1)).toBeCloseTo(0.5);
+  });
+
+  it('Guassian functions', () => {
+    expect(getGaussianFactor()).toBeCloseTo(3.8833175701198104);
   });
 });
