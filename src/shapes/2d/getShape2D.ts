4e34be7de33ae0770868342b404ef01bc7229f53
Michaël Zasso <targos@protonmail.com>
2026-07-09 08:40:36 +0200

    refactor: remove type hacks in 2D

--- src/shapes/2d/getShape2D.ts
+++ src/shapes/2d/getShape2D.ts
@@ -3,21 +3,19 @@ import type { Shape2DInstance } from './Shape2DInstance.ts';
 import { Gaussian2D } from './gaussian2D/Gaussian2D.ts';
 
 /**
- * Generate a instance of a specific kind of shape.
+ * Generate an instance of a specific kind of shape.
  * @param shape - shape descriptor specifying the kind and parameters.
  * @returns an instance of the requested 2D shape class.
  */
 export function getShape2D<TShape extends Shape2D>(
   shape: TShape,
-): Shape2DInstance<TShape['kind']> {
+): Shape2DInstance<TShape['kind']>;
+export function getShape2D(shape: Shape2D) {
   const { kind } = shape;
   switch (kind) {
     case 'gaussian':
       return new Gaussian2D(shape);
-    default: {
-      const unHandled: never = kind;
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      throw new Error(`Unknown distribution ${unHandled}`);
-    }
+    default:
+      throw new Error(`Unknown distribution ${kind as string}`);
   }
 }
